c6554028290291686589f7961379a574c49bff2e
Ed Escalante <edalbert.escalante@gmail.com>
2013-06-20 02:07:47 +0800

    Ajax will fire up only when username length is more than 3

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,12 +1,15 @@
 
 $("#user_username").on("blur", function(){
   var username = $("#user_username").val();
-  $.get("/users/username_taken?username=" + username, function(data){
-    if(data){
-      $("#icon-container").html("<i class='icon-remove text-error'></i>");
+  
+  if(username.length() > 3){
+    $.get("/users/username_taken?username=" + username, function(data){
+      if(data){
+        $("#icon-container").html("<i class='icon-remove text-error'></i>");
+        }
+      else{
+        $("#icon-container").html("<i class='icon-ok text-success'></i>");
       }
-    else{
-      $("#icon-container").html("<i class='icon-ok text-success'></i>");
-    }
-  });
+    });
+  }
 });
